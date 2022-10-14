0e4fbb0ed2e170dbe6cec1983d57e68e30c22084
rzhbit <renzehuibit@163.com>
2022-10-14 11:00:39 +0800

    Update test_install.sh

--- test_install.sh
+++ test_install.sh
@@ -96,7 +96,7 @@ if [ "$only_config" != true ]; then
 
         #install v2ray-plugin
         snap install go --classic
-        wget https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.2/v2ray-plugin-linux-amd64-v1.3.2.tar.gz
+        wget https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.1/v2ray-plugin-linux-amd64-v1.3.1.tar.gz
         tar -zxvf v2ray-plugin-linux-amd64-*.tar.gz
         mv v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin
 
